1e9912abc11c59c192283b4af7f330a138a63bba
Martinet Lee <S469LEE@uwaterloo.ca>
2019-02-02 03:55:11 -0500

    Updating test for empty.sol and fixing test for bin

--- tests/test_usolc.py
+++ tests/test_usolc.py
@@ -26,13 +26,13 @@ def test_semver_filter(sample_version_list, rule_text, expected_result):
     assert(result == expected_result)
 
 
-@pytest.mark.parametrize("filename,expected_line",[
-    ("exactly_one.sol","pragma solidity 0.4.18;\n"),
-    ("caret_0.4.sol","pragma solidity ^0.4.18;\n"),
-    ("range.sol","pragma solidity >=0.4.22 <0.6.0;\n"),
-    ("range_or_one.sol","pragma solidity 0.4.21 || >=0.4.25 <0.6.0;\n"),
+@pytest.mark.parametrize("filename,expected_line", [
+    ("exactly_one.sol", "pragma solidity 0.4.18;\n"),
+    ("caret_0.4.sol", "pragma solidity ^0.4.18;\n"),
+    ("range.sol", "pragma solidity >=0.4.22 <0.6.0;\n"),
+    ("range_or_one.sol", "pragma solidity 0.4.21 || >=0.4.25 <0.6.0;\n"),
 ])
-def test_extract_pragma_line(filename,expected_line):
+def test_extract_pragma_line(filename, expected_line):
     """ Test extract_pragma_line for cases that there both the file and the pragma line exist """
     extracted_line = extract_pragma_line("resources/"+filename)
     assert(expected_line == extracted_line) 
@@ -53,11 +53,11 @@ def test_extract_pragma_line_throws_file_not_found():
         extract_pragma_line("from_somerandomfilenamethat_shouldnt_exist.sol")
 
 
-@pytest.mark.parametrize("pragma, expected_rule",[
+@pytest.mark.parametrize("pragma, expected_rule", [
     ("pragma solidity 0.4.18;\n", "0.4.18"),
-    ("pragma solidity ^0.4.18;\n","^0.4.18"),
-    ("pragma solidity >=0.4.22 <0.6.0;\n",">=0.4.22 <0.6.0"),
-    ("pragma solidity 0.4.21 || >=0.4.25 <0.6.0;\n","0.4.21 || >=0.4.25 <0.6.0"),       
+    ("pragma solidity ^0.4.18;\n", "^0.4.18"),
+    ("pragma solidity >=0.4.22 <0.6.0;\n", ">=0.4.22 <0.6.0"),
+    ("pragma solidity 0.4.21 || >=0.4.25 <0.6.0;\n", "0.4.21 || >=0.4.25 <0.6.0"),
 ])
 def test_getrule_from_pragma(pragma, expected_rule):
     """ Test getrule_from_pragma """
@@ -65,27 +65,28 @@ def test_getrule_from_pragma(pragma, expected_rule):
     assert(expected_rule == extracted_rule) 
 
 
-@pytest.mark.parametrize("filename,expected_rule",[
-    ("resources/exactly_one.sol","0.4.18"),
-    ("resources/caret_0.4.sol","^0.4.18"),
-    ("resources/range.sol",">=0.4.22 <0.6.0"),
-    ("resources/range_or_one.sol","0.4.21 || >=0.4.25 <0.6.0"),       
+@pytest.mark.parametrize("filename,expected_rule", [
+    ("resources/exactly_one.sol", "0.4.18"),
+    ("resources/caret_0.4.sol", "^0.4.18"),
+    ("resources/range.sol", ">=0.4.22 <0.6.0"),
+    ("resources/range_or_one.sol", "0.4.21 || >=0.4.25 <0.6.0"),
+    ("resources/empty.sol", "*"),
 ])
-def test_getrule_from_file(filename,expected_rule):
+def test_getrule_from_file(filename, expected_rule):
     """ Test getrule_from_fiole """
     extracted_rule = getrule_from_file(filename)
     assert(expected_rule == extracted_rule) 
 
 
-@pytest.mark.parametrize("sys_argv, expected_result",[
-    (["solc", "hello.sol", "-U", "0.4.2+" , "--abi", "hello", "-uinfo"],
-     ["hello.sol", ["0.4.2", VersionChoosing.NEWEST], ["hello.sol","--abi", "hello"]]),
+@pytest.mark.parametrize("sys_argv, expected_result", [
+    (["solc", "hello.sol", "-U", "0.4.2+", "--abi", "hello", "-uinfo"],
+     ["hello.sol", ["0.4.2", VersionChoosing.NEWEST], ["hello.sol", "--abi", "hello"]]),
 
-    (["solc", "hello.sol", "-U", "0.4.2+" , "--abi"],
-     ["hello.sol", ["0.4.2", VersionChoosing.NEWEST],["hello.sol","--abi"]]),
+    (["solc", "hello.sol", "-U", "0.4.2+", "--abi"],
+     ["hello.sol", ["0.4.2", VersionChoosing.NEWEST], ["hello.sol", "--abi"]]),
 
     (["solc", "hello.sol", "--abi"],
-     ["hello.sol", ["*",VersionChoosing.NEWEST], ["hello.sol","--abi"]]),
+     ["hello.sol", ["*", VersionChoosing.NEWEST], ["hello.sol", "--abi"]]),
 
     (["solc", "hello.sol"],
      ["hello.sol", ["*", VersionChoosing.NEWEST], ["hello.sol"]]),
@@ -99,7 +100,7 @@ def test_extract_arguments(sys_argv, expected_result):
     assert(expected_result == extracted_result)   
 
 
-@pytest.mark.parametrize("strategy_string, expected_result",[
+@pytest.mark.parametrize("strategy_string, expected_result", [
     ("^0.4.1+", ["^0.4.1", VersionChoosing.NEWEST]),
     (">=0.4.1 <0.4.23", [">=0.4.1 <0.4.23", VersionChoosing.NEWEST]),
     (">=0.4.1 <0.4.23+", [">=0.4.1 <0.4.23", VersionChoosing.NEWEST]),
@@ -114,7 +115,7 @@ def test_interpret_strategy_string(strategy_string, expected_result):
 
 @pytest.mark.parametrize("version_list, expected_version", [
     (["0.3.9", "0.4.1", "0.4.2", "0.4.3", "0.4.18", "0.5.0", "1.0.0", "1.0.1"], "0.3.9"),
-    (["1.0.1","0.4.1", "0.3.9", "0.4.2", "0.4.3", "0.4.18", "0.5.0", "1.0.0"],  "0.3.9"),
+    (["1.0.1", "0.4.1", "0.3.9", "0.4.2", "0.4.3", "0.4.18", "0.5.0", "1.0.0"],  "0.3.9"),
 ])
 def test_semver_min_satisfying_normal(version_list, expected_version):
     """ Test semver_min_satisfying when there should be a result_version """
@@ -129,7 +130,7 @@ def test_semver_min_satisfying_none(sample_version_list):
 
 
 @pytest.mark.parametrize("version_selection_strategy, expected_version", [
-    (["^0.4.1", VersionChoosing.NEWEST],"0.4.18"),
+    (["^0.4.1", VersionChoosing.NEWEST], "0.4.18"),
     (["^0.4.1", VersionChoosing.OLDEST], "0.4.1"),
 ])
 def test_choose_version_by_strategy(sample_version_list,
@@ -202,11 +203,11 @@ def test_run_solc():
     assert(process_return.returncode == 0)
 
 
-@pytest.mark.parametrize("sys_argv, expected_bin_file",[
-    (["solc", "resources/caret_0.4.sol", "--bin", "-o", "test_bin", "-U", "0.4.25", "-uinfo"],
+@pytest.mark.parametrize("sys_argv, expected_bin_file", [
+    (["solc", "resources/caret_0.4.sol", "--bin", "-o", "test_bin_1", "-U", "0.4.25", "-uinfo"],
      "resources/caret_0.4.25.bin"),
-    (["solc", "resources/caret_0.5.sol", "--bin", "-o", "test_bin", "-U", "0.5.0"],
-     "resources/caret_0.4.25.bin"),
+    (["solc", "resources/caret_0.5.sol", "--bin", "-o", "test_bin_2", "-U", "0.5.0"],
+     "resources/caret_0.5.0.bin"),
 ])
 def test_main(sys_argv, expected_bin_file):
     """
@@ -216,13 +217,11 @@ def test_main(sys_argv, expected_bin_file):
     sys.argv = sys_argv
     assert(main() == 0)
     expected_bin_file = [elem.rstrip('\n') for elem in list(open(expected_bin_file, "r"))]
-    produced_bin_file = list(open("test_bin/Ballot.bin", "r"))
+    produced_bin_file = list(open((sys_argv[4] + "/Ballot.bin"), "r"))
     assert(expected_bin_file == produced_bin_file)
 
 
-
-@pytest.mark.parametrize("sys_argv",[
-    (["solc", "resources/empty.sol", "--bin", "-o", "test_bin", "-U", "0.4.25"]),
+@pytest.mark.parametrize("sys_argv", [
     (["solc", "some_random_file_should_not_exist.sol", "--bin", "-o", "test_bin", "-U", "0.5.0"]),
     (["solc", "resources/exactly_0.6.0.sol", "--bin", "-o", "test_bin"]),
     (["solc", "resources/caret_0.5.sol", "--bin", "-o", "test_bin", "-U", "0.4.25"]),
